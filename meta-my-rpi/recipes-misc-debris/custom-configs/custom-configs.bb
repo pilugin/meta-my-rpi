d305e7d877c89c1c665b2526d22790f0853d48bb
Kostiantyn Piliuhin <kpiliuhin@argo.ai>
2020-12-08 11:11:24 +0100

    - keep default HDMI audio sink instead of line out - add blinking to sound2led service

--- meta-my-rpi/recipes-misc-debris/custom-configs/custom-configs.bb
+++ meta-my-rpi/recipes-misc-debris/custom-configs/custom-configs.bb
@@ -8,24 +8,16 @@ PR = "r0"
 
 SRC_URI = "\
 file://wlan0.network \
-file://asound.conf \
 file://session \
 "
 
-FILES_${PN} = "\
-/etc/systemd/network/wlan0.network \
-/home/root/.asoundrc \
-/etc/mini_x/session \
-"
+FILES_${PN} = "*"
 
 S = "${WORKDIR}"
 
 do_install() {
     install -d ${D}/etc/systemd/network
     install -m 0644 ${S}/wlan0.network ${D}/etc/systemd/network/wlan0.network
-
-    install -d ${D}/home/root
-    install -m 0644 ${S}/asound.conf ${D}/home/root/.asoundrc
 
     install -d ${D}/etc/mini_x
     install -m 0755 ${S}/session ${D}/etc/mini_x/session
--- meta-my-rpi/recipes-misc-debris/sound2led/files/sound2led.sh
+++ meta-my-rpi/recipes-misc-debris/sound2led/files/sound2led.sh
@@ -3,18 +3,30 @@
 LED_ON_CMD="mdio-tool w eth0 0x15 0x155"
 LED_OFF_CMD="mdio-tool w eth0 0x15 0x044"
 
-${LED_OFF_CMD}
+function blink() {
+    COUNT=${1}
+    ON_DURATION=${2}
+    OFF_DURATION=${3:-${ON_DURATION}}
+
+    for i in $(seq ${COUNT})
+    do
+        ${LED_ON_CMD}
+        sleep ${ON_DURATION}
+
+        ${LED_OFF_CMD}
+        sleep ${OFF_DURATION}
+    done
+}
+
 trap "${LED_OFF_CMD} ; exit" EXIT INT TERM QUIT HUP
+
+blink 20 0.05 0.05
 
 while true
 do
     if grep -i running /proc/asound/card*/pcm*/sub*/status > /dev/null
     then
-
-        ${LED_ON_CMD}
-        sleep 10
-        ${LED_OFF_CMD}
-
+        blink 20 0.25 0.25
     fi
 
     sleep 0.25
